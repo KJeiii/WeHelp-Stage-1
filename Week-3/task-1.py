5f35354105a8b987d0b8425e9dd449f9568620e5
KJeiii <pleasemailtokj@gmail.com>
2023-07-18 20:21:01 +0800

    complete task-1

--- Week-3/task-1.py
+++ Week-3/task-1.py
@@ -9,7 +9,8 @@ with request.urlopen(url) as response:
     # get list in the json data
     results = json.load(response)['result']['results']
 
-csv_list = []
+# ---- build attraction.csv ----
+attraction_csv_list = []
 for element in results:
 
     # clean address and extract distric sting
@@ -23,11 +24,56 @@ for element in results:
 
     # create a list and store whole data
     list = [element['stitle'], distric, element['longitude'], element['latitude'], img_url]
-    csv_list.append(list)
+    attraction_csv_list.append(list)
 
-# save csv_list in csv file
+# save attraction_csv_list in csv file
 with open("attraction.csv","wt") as file:
     csv_file = csv.writer(file)
-    csv_file.writerows(csv_list)
+    csv_file.writerows(attraction_csv_list)
+
+
+# ---- build mrt.csv ----
+# find all MRT in the JSON data and store in a list
+mrt_list = [ element['MRT'] for element in results ]
+
+# remove duplicate
+unique_mrt_list = []
+for mrt in mrt_list:
+
+    # if mrt is None, do not append
+    if mrt == None:
+        continue
+
+    if mrt not in unique_mrt_list:
+        unique_mrt_list.append(mrt)
+
+# match each mrt in JSON with unique_mrt_list and build mrt csv_list
+mrt_csv_lsit = []
+for mrt in unique_mrt_list:
+    mrt_attraction_list = [mrt]
+    for element in results:
+        if element['MRT'] == mrt:
+            mrt_attraction_list.append(element['stitle'])
+    mrt_csv_lsit.append(mrt_attraction_list)
+
+# save mrt_csv_list in csv file
+with open("mrt.csv",'wt') as file:
+    csv_file = csv.writer(file)
+    csv_file.writerows(mrt_csv_lsit)
+
+
+# confirm whether code loses attraction,
+# there is a None in MRT data, mrt.csv does not contain this attraction.
+length = [len(element)-1 for element in mrt_csv_lsit]
+
+attraction_amount = 0
+for value in length:
+    attraction_amount += value
+
+print(f'Total amount of attraction in Tapei government web : {len(results)}')
+print(f'Total amount of attraction in mrt.csv : {attraction_amount},\nthere is {len(results) - attraction_amount} null data in the web.')
+
+
+
 
 
