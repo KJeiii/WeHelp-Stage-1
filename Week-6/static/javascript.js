ab3551495422339ebeeb2c9ba6f0154a63a36ef3
Mark Chang <109458440+KJeiii@users.noreply.github.com>
2023-08-10 21:54:57 +0800

    Delete Week-6 directory

--- Week-6/static/javascript.js
+++
@@ -1,9 +0,0 @@
-const Check = (cssSelector) => {
-    let inputArray = document.querySelectorAll(cssSelector);
-    for (input of inputArray) {
-        if (input.value === "") {
-            // block form sending info to server 
-            event.preventDefault();
-        };
-    };
-};
